fed8fdb57b3e35fac36339409dd4710e39c58ca3
Hoda0123 <43361728+Hoda0123@users.noreply.github.com>
2018-09-21 05:19:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -233,27 +233,27 @@ client.on('message', message => {
     } else
     
      
-    if (message.content.startsWith(prefix + 'streem')) {
+    if (message.content.startsWith(prefix + 'str')) {
     if (message.author.id !== '468804935643824128') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
     client.user.setGame(argresult, "http://twitch.tv/HA");
         message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
     } else
     
-    if (message.content.startsWith(prefix + 'setname')) {
+    if (message.content.startsWith(prefix + 'name')) {
     if (message.author.id !== '468804935643824128') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
       client.user.setUsername(argresult).then
           message.channel.sendMessage(`**${argresult}** : تم تغير الأسم`)
       return message.reply("**لا تستطيع تغير الأسم الا بعد ساعتين**");
     } else
         
-    if (message.content.startsWith(prefix + 'setavatar')) {
+    if (message.content.startsWith(prefix + 'ava')) {
     if (message.author.id !== '468804935643824128') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
     client.user.setAvatar(argresult);
         message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
     } else
     
     
-    if (message.content.startsWith(prefix + 'watching')) {
+    if (message.content.startsWith(prefix + 'wat')) {
     if (message.author.id !== '468804935643824128') return message.reply('** هذا الأمر فقط لصاحب البوت و شكراًً **')
         client.user.setActivity(argresult, {type : 'watching'});
      message.channel.sendMessage(`**${argresult}** : تم تغيير الووتشينق الى`)
