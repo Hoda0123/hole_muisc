0d001c675e94359841a53a38d85fd1d8801de7b8
Hoda0123 <43361728+Hoda0123@users.noreply.github.com>
2019-05-30 15:39:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -223,7 +223,7 @@ client.on('message', message => {
       if (!message.content.startsWith(prefix)) return;
       var args = message.content.split(' ').slice(1);
       var argresult = args.join(' ');
-      if (message.author.id == 410835593451405312) return;
+      if (message.author.id == 491511311423766531) return;
     
     
     if (message.content.startsWith(prefix + 'playing')) {
